70ecf6c8cb77e1273a0f61ad53b0030562f26874
Edoardo Negri <edne@gmx.com>
2015-09-16 12:14:17 +0200

    arrays

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,23 +1,9 @@
 #include <TimerOne.h>
 
 #define PIN_LOOP(_var) for(byte _var=2; _var<=11; _var+=2)
+
 #define RESOLUTION 40 //Microsecond resolution for notes
-
-
-/*NOTE: Many of the arrays below contain unused indexes.  This is
-  to prevent the Arduino from having to convert a pin input to an alternate
-  array index and save as many cycles as possible.  In other words information
-  for pin 2 will be stored in index 2, and information for pin 4 will be
-  stored in index 4.*/
-
-
-/*An array of maximum track positions for each step-control pin.  Even pins
-  are used for control, so only even numbers need a value here.  3.5" Floppies have
-  80 tracks, 5.25" have 50.  These should be doubled, because each tick is now
-  half a position (use 158 and 98).
- */
-byte MAX_POSITION[] = {
-    0,0,158,0,158,0,158,0,158,0,158,0,158,0,158,0,158,0};
+#define MAX_POSITION 158
 
 //Array to track the current position of each floppy head.  (Only even indexes (i.e. 2,4,6...) are used)
 byte current_position[] = {
@@ -26,7 +12,7 @@ byte current_position[] = {
 /*Array to keep track of state of each pin.  Even indexes track the control-pins for toggle purposes.  Odd indexes
   track direction-pins.  LOW = forward, HIGH=reverse
  */
-int current_state[] = {
+int pin_state[] = {
     0,0,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW,LOW
 };
 
@@ -56,7 +42,6 @@ void setup() {
     Serial.begin(9600);
 
     reset_all();
-    digitalWrite(13, LOW);
 }
 
 
@@ -66,7 +51,6 @@ void loop() {
     if (Serial.available() > 2) {
         //Watch for special 100-message to reset the drives
         if (Serial.peek() == 100) {
-            digitalWrite(13, HIGH);
             reset_all();
             //Flush any remaining messages.
             while(Serial.available() > 0) {
@@ -101,17 +85,17 @@ void tick() {
 
 void toggle_pin(byte pin, byte direction) {
     //Switch directions if end has been reached
-    if (current_position[pin] >= MAX_POSITION[pin]) {
-        current_state[direction] = HIGH;
-        digitalWrite(direction,HIGH);
+    if (current_position[pin] >= MAX_POSITION) {
+        pin_state[direction] = HIGH;
+        digitalWrite(direction, HIGH);
     }
     else if (current_position[pin] <= 0) {
-        current_state[direction] = LOW;
-        digitalWrite(direction,LOW);
+        pin_state[direction] = LOW;
+        digitalWrite(direction, LOW);
     }
 
     //Update current_position
-    if (current_state[direction] == HIGH) {
+    if (pin_state[direction] == HIGH) {
         current_position[pin]--;
     }
     else {
@@ -119,8 +103,8 @@ void toggle_pin(byte pin, byte direction) {
     }
 
     //Pulse the control pin
-    digitalWrite(pin,current_state[pin]);
-    current_state[pin] = ~current_state[pin];
+    pin_state[pin] = ~pin_state[pin];
+    digitalWrite(pin, pin_state[pin]);
 }
 
 
@@ -134,16 +118,16 @@ void reset_all() {
     // New all-at-once reset
     for (byte s=0; s<80;s++) { // For max drive's position
         PIN_LOOP(p) {
-            digitalWrite(p+1,HIGH); // Go in reverse
-            digitalWrite(p,HIGH);
-            digitalWrite(p,LOW);
+            digitalWrite(p+1, HIGH); // Go in reverse
+            digitalWrite(p, HIGH);
+            digitalWrite(p, LOW);
         }
         delay(5);
     }
 
     PIN_LOOP(p) {
         current_position[p] = 0; // We're reset.
-        digitalWrite(p+1,LOW);
-        current_state[p+1] = 0; // Ready to go forward.
+        digitalWrite(p+1, LOW);
+        pin_state[p+1] = 0; // Ready to go forward.
     }
 }
